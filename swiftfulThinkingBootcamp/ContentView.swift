cb2f51d305e4780ab3f83fecd26ea66fa042966d
Timi Ajeigbe <timiajeigbe@gmail.com>
2022-06-13 02:31:23 +0100

    Initial Commit

---
+++ swiftfulThinkingBootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftfulThinkingBootcamp
+//
+//  Created by Timi Ajeigbe on 13/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftfulThinkingBootcamp/swiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulThinkingBootcampApp.swift
+//  swiftfulThinkingBootcamp
+//
+//  Created by Timi Ajeigbe on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
